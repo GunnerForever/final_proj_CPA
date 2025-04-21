becc8878e5e40f39d9e2a76e2274cbdb60d24d91
DavidNaranja <david_naranjo@brown.edu>
2025-04-21 17:02:21 -0400

    Copy and pasted code from prior homework to make a baseline for the actual implementation

--- assignment.py
+++ assignment.py
@@ -11,12 +11,12 @@ import math
 # ensures that we run only on cpu
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-def train_model(model, captions, img_feats, pad_idx, args, valid):
+def train_model(model, data, pad_idx, args, valid):
     '''Trains model and returns model statistics'''
     stats = []
     try:
         for epoch in range(args.epochs):
-            stats += [model.train(captions, img_feats, pad_idx, batch_size=args.batch_size)]
+            stats += [model.train(data, batch_size=args.batch_size)]
             if args.check_valid:
                 model.test(valid[0], valid[1], pad_idx, batch_size=args.batch_size)
     except KeyboardInterrupt as e:
@@ -34,55 +34,29 @@ def test_model(model, captions, img_feats, pad_idx, args):
     return perplexity, accuracy
 
 def visualize_loss(losses):
-    """
-    Uses Matplotlib to visualize the losses of our model.
-    :param losses: list of loss data stored from train. Can use the model's loss_list
-    field
-    NOTE: DO NOT EDIT
-    :return: doesn't return anything, a plot should pop-up
-    """
-    x = [i for i in range(len(losses))]
-    plt.plot(x, losses)
-    plt.title('Loss per batch')
-    plt.xlabel('Batch')
-    plt.ylabel('Loss')
-    plt.show()
+    pass
 
 def visualize_results():
     pass
 
 def main(epochs):
-    '''
-    Read in CIFAR10 data (limited to 2 classes), initialize your model, and train and 
-    test your model for a number of epochs. We recommend that you train for
-    10 epochs and at most 25 epochs.
-
-    Consider printing the loss, training accuracy, and testing accuracy after each epoch
-    to ensure the model is training correctly.
-    
-    Students should receive a final accuracy 
-    on the testing examples for cat and dog of >=70%.
-    
-    :return: None
-    '''
-
     LOCAL_TRAIN_FILE = 'data/train'
     LOCAL_TEST_FILE = 'data/test'
 
     CLASSES = [3, 5]
     # TODO: assignment.main() pt 1
-    # Load your testing and training data using the get_data function
+    # Load testing and training data using the get_data function
 
     data = get_data(LOCAL_TRAIN_FILE, CLASSES)
     test_data = get_data(LOCAL_TEST_FILE, CLASSES)
 
-    # Initialize your model and optimizer
+    # Initialize model and optimizer
     #model = 
     optimizer = tf.optimizers.Adam(0.003)
-    # Train your model
+    # Train model
     for i in range(epochs):
         train(model, optimizer, train_imps, train_labradors)
-    # Test your model
+    # Test model
     acc = test(model, test_imps, test_labradors)
     print("Final Acc: " + str(acc))
     visualize_loss(model.loss_list)
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 #@tf.keras.saving.register_keras_serializable(package="ImCapModel")
 class CpaModel(tf.keras.Model):
-    def __init__(self, decoder, embeddings, dosage_encoder, **kwargs):
+    def __init__(self, decoder, embeddings, **kwargs):
         super().__init__(**kwargs)
         self.embeddings = embeddings
         self.decoder = decoder
@@ -14,6 +14,16 @@ class CpaModel(tf.keras.Model):
 
         #Put them through the "composition" part of the autoencoder
         composite = self.composition(basal_state, pert_embed, cov_embed)
+
+        #Decode
+        return self.decoder(composite)  
+    
+    def call(self, cell_state, perts, covs, new_perts): #Function overloading for test case
+        #Get Z_basal, apply V_perturbations and V_covariates
+        basal_state, pert_embed, cov_embed = self.embeddings.call(cell_state, perts, covs)
+
+        #Put them through the "composition" part of the autoencoder
+        composite = self.composition(basal_state, pert_embed, new_perts)
 
         #Decode
         return self.decoder(composite)  
@@ -34,71 +44,36 @@ class CpaModel(tf.keras.Model):
     def train(self, data, batch_size=10):
         for index, end in enumerate(range(batch_size, len(data)+1, batch_size)):
             ## Get batches of data, convert into (cell state, perturbations, covariates) triplets
-
+            cell_state, perts, covs = (-1, -1, -1)
 
             ## Perform a training forward pass. Make sure to factor out irrelevant labels.
             with tf.GradientTape() as tape:
-                #probs = self(cell)
-                loss = self.loss_function(probs, decoder_labels, mask)
+                decoded_z = self(cell_state, perts, covs)
+                loss = self.loss_function(decoded_z)
 
             ## Perform a backwards pass
             gradients = tape.gradient(loss, self.trainable_variables)
             self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
 
-    def test(self,  batch_size=30):
-        """
-        Runs through one epoch - all testing examples.
+    def test(self, data, new_pertubations, batch_size=30):
+        #num_batches = int(len(test_captions) / batch_size)
 
-        :param model: the initilized model to use for forward and backward pass
-        :param test_captions: test caption data (all data for testing) of shape (num captions,20)
-        :param test_image_features: test image feature data (all data for testing) of shape (num captions,1000)
-        :param padding_index: the padding index, the id of *PAD* token. This integer is used to mask padding labels.
-        :returns: perplexity of the test set, per symbol accuracy on test set
-        """
-        num_batches = int(len(test_captions) / batch_size)
-
-        total_loss = total_seen = total_correct = 0
-        for index, end in enumerate(range(batch_size, len(test_captions)+1, batch_size)):
-
-            ## Get the current batch of data, making sure to try to predict the next word
-            start = end - batch_size
-            batch_image_features = test_image_features[start:end, :]
-            decoder_input = test_captions[start:end, :-1]
-            decoder_labels = test_captions[start:end, 1:]
+        total_loss = 0
+        for index, end in enumerate(range(batch_size, len(data)+1, batch_size)):
+            ## Get batches of data, convert into (cell state, perturbations, covariates) triplets and also get the extra pertubations data
+            cell_state, perts, covs = (-1, -1, -1)
 
             ## Perform a no-training forward pass. Make sure to factor out irrelevant labels.
-            probs = self(batch_image_features, decoder_input)
-            mask = decoder_labels != padding_index
-            num_predictions = tf.reduce_sum(tf.cast(mask, tf.float32))
-            loss = self.loss_function(probs, decoder_labels, mask)
-            accuracy = self.accuracy_function(probs, decoder_labels, mask)
+            decoded_z = self(cell_state, perts, covs, new_pertubations)
+            #num_predictions = tf.reduce_sum(tf.cast(mask, tf.float32))
+            #loss = self.loss_function(probs, decoder_labels, mask)
+            #accuracy = self.accuracy_function(probs, decoder_labels, mask)
 
             ## Compute and report on aggregated statistics
-            total_loss += loss
-            total_seen += num_predictions
-            total_correct += num_predictions * accuracy
-
-            avg_loss = float(total_loss / total_seen)
-            avg_acc = float(total_correct / total_seen)
-            avg_prp = np.exp(avg_loss)
-            print(f"\r[Valid {index+1}/{num_batches}]\t loss={avg_loss:.3f}\t acc: {avg_acc:.3f}\t perp: {avg_prp:.3f}", end='')
-
-        print()        
-        return avg_prp, avg_acc
+            avg_acc = -1
+        return avg_acc
 
 
-    def loss_function(prbs, labels, mask):
-        """
-        Calculates the model cross-entropy loss after one forward pass
-        Please use reduce sum here instead of reduce mean to make things easier in calculating per symbol accuracy.
-
-        :param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
-        :param labels:  integer tensor, word prediction labels [batch_size x window_size]
-        :param mask:  tensor that acts as a padding mask [batch_size x window_size]
-        :return: the loss of the model as a tensor
-        """
-        masked_labs = tf.boolean_mask(labels, mask)
-        masked_prbs = tf.boolean_mask(prbs, mask)
-        scce = tf.keras.losses.sparse_categorical_crossentropy(masked_labs, masked_prbs, from_logits=True)
-        loss = tf.reduce_sum(scce)
+    def loss_function(self, decoded_z):
+        loss = -1
         return loss
